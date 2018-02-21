7543845ff2c26b21dfe02edc5f0a02eb0c0ea30e
unknown <had196@inf.elte.hu>
2018-02-21 20:14:21 +0100

    Led is controlled from Azure.

---
+++ lightcontroller/AzureRemoteLight/AzureRemoteLight/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using GalaSoft.MvvmLight;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace AzureRemoteLight.ViewModel
+{
+	class MainViewModel : ViewModelBase
+	{
+		public GpioController gpioController { get; }
+		public GpioPin ledPin { get; }
+
+		public DeviceClient deviceClient { get; }
+
+		public string IotHubUri { get; } = "SmartHomeApplication.azure-devices.net";
+		public string DeviceKey { get; } = "qB1w140QB445Z4zdG5NP04QtKeJohUwzp1R9SEBmRIk=";
+		public string DeviceId => "rpi3";
+
+		public MainViewModel()
+		{
+			deviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey));
+			gpioController = GpioController.GetDefault();
+			if (null != gpioController)
+			{
+				ledPin = gpioController.OpenPin(4);
+				ledPin.SetDriveMode(GpioPinDriveMode.Output);
+			}
+		}
+
+		public async Task SendDeviceToCloudMessagesAsync()
+		{
+			try
+			{
+				var telemetryDataPoint = new
+				{
+					deviceId = DeviceId,
+					message = "Hello"
+				};
+				var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+				var message = new Message(Encoding.ASCII.GetBytes(messageString));
+				await deviceClient.SendEventAsync(message);
+				Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+				//IsAzureConnected = true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
+
+		public async Task ReceiveCloudToDeviceMessageAsync()
+		{
+			Debug.WriteLine("\nReceiving cloud to device messages from service");
+			while (true)
+			{
+				Message receivedMessage = await deviceClient.ReceiveAsync();
+				if (receivedMessage == null) continue;
+				var msg = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+				if (msg == "on")
+				{
+					ledPin.Write(GpioPinValue.Low);
+				}
+				if (msg == "off")
+				{
+					ledPin.Write(GpioPinValue.High);
+				}
+				await deviceClient.CompleteAsync(receivedMessage);
+			}
+		}
+	}
+}
---
+++ lightcontroller/LightController/LightController/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LightController
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		static ServiceClient serviceClient;
+		static string connectionString = "HostName=SmartHomeApplication.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=3JE/ceW92b8Gkp79sRz0OAJxmm5KV7vQQ0ADqQOoQ70=";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+		}
+		private async Task TurnLight(bool isOn)
+		{
+			await SendCloudToDeviceMessageAsync(isOn);
+		}
+		private static async Task SendCloudToDeviceMessageAsync(bool isOn)
+		{
+			var commandMessage = new Message(Encoding.ASCII.GetBytes(isOn ? "on" : "off"));
+			await serviceClient.SendAsync("rpi3", commandMessage);
+		}
+		private async void BtnTurnOn_OnClick(object sender, RoutedEventArgs e)
+		{
+			await TurnLight(true);
+		}
+		private async void BtnTurnOff_OnClick(object sender, RoutedEventArgs e)
+		{
+			await TurnLight(false);
+		}
+	}
+}
