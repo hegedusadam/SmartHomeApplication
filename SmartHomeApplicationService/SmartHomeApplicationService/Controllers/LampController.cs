ecb47b052871eb4648f1d8eb3987653c85680cbe
hegedusadam <had196@inf.elte.hu>
2018-03-04 22:44:58 +0100

    Add SignalR to the project, create LampHub and LampController.

---
+++ SmartHomeApplicationService/SmartHomeApplicationService/Controllers/LampController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using SmartHomeApplicationService.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SmartHomeApplicationService.Controllers
+{
+    public class LampController : ApiController
+    {
+		public void Get(bool TurnOn)
+		{
+			var lampContext = GlobalHost.ConnectionManager.GetHubContext<LampHub>();
+
+			LampHub.TurnOn = TurnOn;
+			lampContext.Clients.All.OnSwitch(TurnOn);
+		}
+    }
+}
