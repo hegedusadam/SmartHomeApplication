52f248888bb088672a905762e6f5ffe1793a34cc
hegedusadam <had196@inf.elte.hu>
2018-03-29 10:53:20 +0200

    Create ClientUWP project, add LampSwitch View and ViewModel.

---
+++ SmartHomeApplicationService/SmartHomeApplication.ClientUWP/ViewModel/LampSwitchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeApplication.ClientUWP.ViewModel
+{
+	class LampSwitchViewModel
+	{
+	}
+}
